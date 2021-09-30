e84e57eee1136c68fc32daeae6e6adb468aa23e9
윤정훈 <yunjunghun0116@gmail.com>
2021-09-30 18:57:27 +0900

    set up

---
+++ src/screens/HomeScreen/home_screen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeScreen() {
+  return (
+    <div style={{ height: "3000px", backgroundColor: "green" }}>
+      homescreen;
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/screens/ProfileScreen/profile_screen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProfileScreen() {
+  return <div>profilescreen</div>;
+}
+
+export default ProfileScreen;
