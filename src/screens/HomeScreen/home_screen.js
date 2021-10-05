228efab58f51c417c2d4fb261601b181967c2640
윤정훈 <yunjunghun0116@gmail.com>
2021-10-05 19:06:26 +0900

    image는 임시로 넣었음

--- src/screens/HomeScreen/home_screen.js
+++ src/screens/HomeScreen/home_screen.js
@@ -1,38 +1,100 @@
 import React from "react";
 import "./home_screen.css";
+import Image from "../../images/logo.png";
+import IconImage from "../../images/outline_expand_more_black_48dp.png";
+import emailImage from "../../images/outline_email_black_48dp.png";
+import phoneImage from "../../images/outline_call_black_48dp.png";
+import socialImage from "../../images/outline_public_black_48dp.png";
 
 function HomeScreen() {
-  let my_height = window.innerHeight + "px";
+  function ScrollDown() {
+    const current_scroll = window.scrollY;
+    const my_height = window.innerHeight;
+
+    window.scrollTo(current_scroll, my_height);
+  }
   return (
     <div className="home_main">
-      <div style={{ height: my_height }} className="home_left">
-        <div id="center_div">
-          <div style={{ fontSize: "50px", color: "#3F4346" }}>Mealacle</div>
-          <div className="home_left_img"></div>
-          <div
-            style={{ fontSize: "30px", color: "#3F4346", fontWeight: "10px" }}
-          >
-            코로나 극복을 위해 Mealacle과 함께 기적을 일으킵시다!
+      {/* 1번 페이지  */}
+      <div className="home_main__top">
+        {/* 왼쪽 로고 및 타이틀  */}
+        <div className="home_left">
+          <div id="home_left__center">
+            <div>Mealacle</div>
+            <img src={Image} id="logo_img" alt="Logo" />
+            <div>코로나 극복을 위해 Mealacle과 함께 기적을 일으킵시다!</div>
           </div>
         </div>
-      </div>
-      <div style={{ height: my_height }} className="home_right">
-        <div id="sign_in_div">
-          <div className="sign_in_store_code">
-            <div className="store_code_input">
-              <label>매장코드</label>
+        {/* 로그인 창  */}
+        <div className="home_right">
+          <div id="sign_in_div">
+            <div className="sign_in_store_code">
+              <div className="store_code_input">
+                <label>매장코드</label>
+              </div>
+              <div className="input_area_div">
+                <input type="text" placeholder="매장 코드를 입력하세요." />
+              </div>
             </div>
-            <div className="input_area_div">
-              <input type="text" />
+            <div className="sign_in_password">
+              <div className="password_input">
+                <label>비밀번호</label>
+              </div>
+              <div className="input_area_div">
+                <input type="text" placeholder="비밀번호를 입력하세요." />
+              </div>
             </div>
           </div>
-          <div className="sign_in_password">
-            <div className="password_input">
-              <label>비밀번호</label>
+        </div>
+        {/* Slide down Icon */}
+        <div id="down-icon" onClick={ScrollDown}>
+          <img src={IconImage} alt="SlideDownIcon" />
+        </div>
+      </div>
+      {/* 2번 페이지  */}
+      <div className="home_main__bottom">
+        <div className="home_bottom_left">
+          <div className="home_bottom_text_container">
+            <h1>Mealacle은?</h1>
+          </div>
+          <div className="home_bottom_text_container">
+            <p>
+              Mealacle은 소상공인 자영업자들과 소비자들의 코로나 팬데믹 극복을
+              위해 새롭게 등장한 통신 판매 유통 모바일 서비스입니다.
+            </p>
+          </div>
+          <div className="home_bottom_text_container">
+            <p>
+              Mealacle은 기존의 밀키트 유통 방법과는 다르게 소비자에 의해
+              소비되고, 소비자에 의해 유통되는 기존의 유통 시스템과는 다른 제
+              3유통 서비스를 제공합니다.
+            </p>
+          </div>
+          <div className="home_bottom_text_container">
+            <p>
+              Mealacle은 코로나 팬데믹이 끝나는 날까지 사회와 모든 사람들을 위해
+              힘쓰는 서비스가 되도록 노력하겠습니다.
+            </p>
+          </div>
+        </div>
+        <div className="home_bottom_right">
+          <div className="connect_container">
+            <div className="circle_image_container">
+              <img src={emailImage} id="email_img" alt="email" width="80%" />
             </div>
-            <div className="input_area_div">
-              <input type="text" />
+            <div className="connect_text_container">이메일 연락</div>
+          </div>
+          <div className="connect_container">
+            <div className="circle_image_container">
+              <img src={phoneImage} id="phone_img" alt="phone" width="80%" />
             </div>
+            <div className="connect_text_container">전화 연락</div>
+          </div>
+          <div className="connect_container">
+            <div className="circle_image_container">
+              <img src={socialImage} id="social_img" alt="social" width="80%" />
+            </div>
+            <div className="connect_text_container">소셜 방문</div>
           </div>
         </div>
       </div>
--- src/screens/ProfileScreen/profile_screen.js
+++ src/screens/ProfileScreen/profile_screen.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./profile_screen.css";
 
 function ProfileScreen() {
-  return <div>profilescreen</div>;
+  return (
+    <div className="profile_main">
+      <div className="profile_maintitle">프로필 선택</div>
+      <div className="profile_subtitle">
+        프로필은 최대 3개까지 생성할 수 있으며 수정이 가능합니다.
+      </div>
+      <div className="profile_card_area">
+        <div className="profile_card">
+          <div>haha</div>
+        </div>
+        <div className="profile_card">
+          <div>haha</div>
+        </div>
+        <div className="profile_card">
+          <div>haha</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileScreen;
