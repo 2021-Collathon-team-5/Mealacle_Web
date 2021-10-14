45a035cb64100aa7d5840c0d0ebbff28ae37e7bf
csk6314 <37896060+csk6314@users.noreply.github.com>
2021-10-14 16:07:36 +0900

    branch to seungkyu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+<<<<<<< Updated upstream
+=======
+import "./styles/styles.css";
+>>>>>>> Stashed changes
 
 ReactDOM.render(
   <React.StrictMode>
