d6a9d151262c80ae60c6c4a3cb1c2ae89df2fa02
csk6314 <37896060+csk6314@users.noreply.github.com>
2021-11-10 02:45:04 +0900

    fuck

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import foodListReducer from "./foods/reducer";
+import orderReducer from "./order/reducer";
 import storeReducer from "./store/reducer";
 
 const rootReducer = combineReducers({
     foods:foodListReducer,
-    store:storeReducer
+    store:storeReducer,
+    order:orderReducer
 });
 
 export default rootReducer;
--- src/redux/store/action.js
+++ src/redux/store/action.js
@@ -1,33 +1,28 @@
-import {
-  GET_PROFILE,
-  SET_NOW_PROFILE,
-  SET_PROFILE,
-  UPDATE_PROFILE,
-} from "./types";
+import { GET_PROFILE, SET_NOW_PROFILE, SET_PROFILE, UPDATE_STORE } from "./types";
 
-export const getProfile = (id, profile) => {
-  return {
-    type: GET_PROFILE,
-    profile,
-    id,
-  };
-};
-export const setProfile = (profile, idx) => {
-  return {
-    type: SET_PROFILE,
-    profile,
-    idx,
-  };
-};
+export const getProfile = (storeID,storeData) => {
+    return {
+        type:GET_PROFILE,
+        storeData,
+        storeID,
+    }
+}
+export const setProfile = (profile,idx) => {
+    return {
+        type:SET_PROFILE,
+        profile,
+        idx
+    }
+}
 export const setNowProfile = (idx) => {
   return {
     type: SET_NOW_PROFILE,
     idx,
   };
 };
-export const updateProfile = (profile) => {
+export const updateStore = (storeName) => {
   return {
-    type: UPDATE_PROFILE,
-    profile,
+    type:UPDATE_STORE,
+    storeName
   };
 };
